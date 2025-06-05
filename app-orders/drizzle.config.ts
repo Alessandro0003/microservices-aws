ecfeffca5c42b0c14b6861c50ca97669f849088e
Alessandro0003 <alessandroquinta3@gmail.com>
2025-06-04 22:10:04 -0300

    🔧 chore: configuration drizzle-orm

--- app-orders/drizzle.config.ts
+++ app-orders/drizzle.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'drizzle-kit'
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL must be defined in the environment variables')
-}
 
 export default defineConfig({
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
-  schema: 'src/db/schema/*',
+  schema: 'src/db/schemas/*',
   out: 'src/db/migrations',
   casing: 'snake_case',
 })
