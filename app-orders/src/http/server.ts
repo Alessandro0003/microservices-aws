69f31d3dbdc237393df6b5a1477925ef3cc93ca3
Alessandro0003 <alessandroquinta3@gmail.com>
2025-06-04 23:33:06 -0300

    :sparkles: feat: add functions to create orders and dispatch message

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,17 +1,22 @@
+import fastifyCors from '@fastify/cors'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider
 } from 'fastify-type-provider-zod'
+import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
+import { db } from '../db/client.ts'
+import { schema } from '../db/schemas/index.ts'
+import { dispatchOrderCreated } from '../rabbitmq/messages/order-created.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
-
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+app.register(fastifyCors, { origin: '*' })
 
 // Essa rote é usada para verificar se o servidor está funcionando corretamente
 app.get('/health', () => {
@@ -27,10 +32,26 @@ app.post('/orders', {
       amount: z.coerce.number(),
     })
   },
-}, (request, reply) => {
+}, async (request, reply) => {
   const { amount } = request.body
 
   console.log('Order created with amount:', amount)
+
+  const orderId = randomUUID()
+
+  await dispatchOrderCreated({
+    orderId,
+    amount,
+    customer: {
+      id: '123e4567-e89b-12d3-a456-426614174000', // Exemplo de ID de cliente
+    }
+  })
+
+  await db.insert(schema.orders).values({
+    id: orderId,
+    customerId: '123e4567-e89b-12d3-a456-426614174000',
+    amount,
+  })
 
   return reply.status(201).send()
 })
