5aa7a79a7cc588820f701317b805d5fde496a6ae
Alessandro0003 <alessandroquinta3@gmail.com>
2025-06-11 22:33:14 -0300

    :bug: fix: passing hostname

--- infra/index.ts
+++ infra/index.ts
@@ -1,6 +1,11 @@
+import * as pulumi from '@pulumi/pulumi';
+import { appLoadBalancer } from './src/load-balancer';
 import { ordersService } from './src/services/orders';
+import { rabbitMQService } from './src/services/rabbitmq';
 
-export const ordersId =  ordersService.service.id;
+export const ordersId = ordersService.service.id;
+const rabbitMQId = rabbitMQService.service.id; 
+export const rabbitMQAdminURL = pulumi.interpolate`http://${appLoadBalancer.listeners[0].endpoint.hostname}:15672`;
 
 
 
