20b73981bcf81314e2a6a5af17b6119f44e4f5e6
Alessandro0003 <alessandroquinta3@gmail.com>
2025-06-11 22:56:34 -0300

    ♻️ refactor: add export to rabbitMQId

--- infra/index.ts
+++ infra/index.ts
@@ -4,7 +4,7 @@ import { ordersService } from './src/services/orders';
 import { rabbitMQService } from './src/services/rabbitmq';
 
 export const ordersId = ordersService.service.id;
-const rabbitMQId = rabbitMQService.service.id; 
+export const rabbitMQId = rabbitMQService.service.id; 
 export const rabbitMQAdminURL = pulumi.interpolate`http://${appLoadBalancer.listeners[0].endpoint.hostname}:15672`;
 
 
